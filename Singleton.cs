62cb41d29c7627f683c519def7799306db86b0e8
thhande <135616907+thhande@users.noreply.github.com>
2025-12-18 17:30:38 +0700

    Update Singleton.cs

--- Singleton.cs
+++ Singleton.cs
@@ -1,3 +1,6 @@
+//for me singleton is the most used design pattern in game development, and i had to rewrite this many times before i made this
+//many tutorials will have the dontdestroy in the last line of awake, but i want some singletons just exsist on its specified scenes. So you will have to add the line yourself by overrding method
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
